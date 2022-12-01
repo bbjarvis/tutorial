d9b8125bf2dd14b9430d02316f70420a98e7c007
bbjarvis <brettbjarvis@gmail.com>
2022-12-01 12:54:45 -0800

    add BookList to index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import ReactDom from 'react-dom'
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h2>John doe</h2>
-const Message = () => {
-  return <p>this is a message</p>
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
 }
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL300_SR300,200_.jpg' />
+)
+
+const Title = () => <h1>It Starts with Us: A Novel (Volume 2)</h1>
+
+const Author = () => <h4>Colleen Hoover</h4>
+
+ReactDom.render(<BookList />, document.getElementById('root'))
