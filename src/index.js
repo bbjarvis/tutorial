7178eff274196d216c797482cfab529de962f324
bbjarvis <brettbjarvis@gmail.com>
2022-12-08 10:36:16 -0800

    add BookList map function

--- src/index.js
+++ src/index.js
@@ -4,43 +4,37 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 // setup vars
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL600_SR600,400_.jpg',
-  title: 'It Starts with Us: A Novel (Volume 2)',
-  author: 'Colleen Hoover',
-}
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51-j3rNPY0L._AC_UL600_SR600,400_.jpg',
-  title:
-    'Things To Do While You Poo On The Loo: Activity Book With Funny Facts, Bathroom Jokes, Poop Puzzles, Sudoku & Much More. Perfect Gag Gift.',
-  author: 'Alex Smart',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL600_SR600,400_.jpg',
+    title: 'It Starts with Us: A Novel (Volume 2)',
+    author: 'Colleen Hoover',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51-j3rNPY0L._AC_UL600_SR600,400_.jpg',
+    title:
+      'Things To Do While You Poo On The Loo: Activity Book With Funny Facts, Bathroom Jokes, Poop Puzzles, Sudoku & Much More. Perfect Gag Gift.',
+    author: 'Alex Smart',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91OBt05JVeL._AC_UL600_SR600,400_.jpg',
+    title: 'Guinness World Records 2023',
+    author: 'Guinness World Records',
+  },
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, est
-          voluptates. Delectus sapiente vero sed laboriosam obcaecati,
-          accusantium est! Similique earum at dolor saepe, alias quibusdam
-          itaque iste ratione molestias.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      ></Book>
+      {books.map((book) => {
+        return <Book book={book}></Book>
+      })}
     </section>
   )
 }
 // destructring props, note that "children" is a specail name that refers to children elements
-const Book = ({ img, title, author, children }) => {
+const Book = (props) => {
+  const { img, title, author, children } = props.book
   return (
     <article className='book'>
       <img src={img} alt='' />
