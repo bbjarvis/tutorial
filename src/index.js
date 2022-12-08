86dbeb824d05d8f16ff959762b09c238c6dab04a
bbjarvis <brettbjarvis@gmail.com>
2022-12-08 11:00:33 -0800

    add onClicks and onMouseOver examples

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ import './index.css'
 // setup vars
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL600_SR600,400_.jpg',
     title: 'It Starts with Us: A Novel (Volume 2)',
     author: 'Colleen Hoover',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/51-j3rNPY0L._AC_UL600_SR600,400_.jpg',
     title:
       'Things To Do While You Poo On The Loo: Activity Book With Funny Facts, Bathroom Jokes, Poop Puzzles, Sudoku & Much More. Perfect Gag Gift.',
     author: 'Alex Smart',
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/91OBt05JVeL._AC_UL600_SR600,400_.jpg',
     title: 'Guinness World Records 2023',
     author: 'Guinness World Records',
@@ -27,20 +30,32 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        return <Book book={book}></Book>
+        return <Book {...book} key={book.id}></Book>
       })}
     </section>
   )
 }
 // destructring props, note that "children" is a specail name that refers to children elements
-const Book = (props) => {
-  const { img, title, author, children } = props.book
+const Book = ({ img, title, author, children }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const complexExample = (e) => {
+    console.log(e)
+    console.log(e.target)
+  }
   return (
-    <article className='book'>
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title)
+      }}
+    >
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
-      {children}
+      <button type='button' onClick={complexExample}>
+        complex example
+      </button>
     </article>
   )
 }
